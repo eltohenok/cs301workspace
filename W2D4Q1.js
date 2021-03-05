472737eb8840d948410fc935e719685aacd40668
eltohenok <79168610+eltohenok@users.noreply.github.com>
2021-03-05 04:38:03 -0800

    Create W2D4Q1.js

---
+++ W2D4Q1.js
@@ -0,0 +1,9 @@
+
+
+    
+        function addend(arr){
+            let sum=arr[0]+arr[arr.length-1];
+            return console.log(sum);
+          }
+          let arr=[1,2,3,4]
+          addend(arr);
