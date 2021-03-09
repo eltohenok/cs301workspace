0c36f30766d3bacb0894d078b705cf061eb1e139
eltohenok <79168610+eltohenok@users.noreply.github.com>
2021-03-09 05:17:53 -0800

    Update w3q6.js

--- w3q6.js
+++ w3q6.js
@@ -1,5 +1,5 @@
 function toTitleCase(str){
-    return str.replace(function(text){
+    return str.replace(function(str){
         return Text.charAt(0).toUpperCase()+Text.substr(1).toTitleCase();
     });
 }
