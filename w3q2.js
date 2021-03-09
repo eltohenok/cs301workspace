6efcbc023856290a666ca95665ac24c1f7291c52
eltohenok <79168610+eltohenok@users.noreply.github.com>
2021-03-09 04:08:17 -0800

    Create w3q2.js

---
+++ w3q2.js
@@ -0,0 +1,17 @@
+let obj1 = {
+    name: "John",
+    age: 30,
+    };
+    let let obj2 = {
+        name: "henok",
+        age: 30,
+        };
+    function isPersonEqual(obj1, obj2){
+        if(obj1===obj2){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    isPersonEqual(obj1,obj2);
