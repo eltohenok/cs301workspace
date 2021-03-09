1f0051df35da80895edfc238b6efb95cf70665a9
eltohenok <79168610+eltohenok@users.noreply.github.com>
2021-03-09 04:30:43 -0800

    Create w3q3.js

---
+++ w3q3.js
@@ -0,0 +1,9 @@
+let obj={};
+function countProperties(obj){
+    let count;
+    for(let i=0;i<Object.countProperties;i++){
+        count ++
+    }
+    return count;
+}
+countProperties(object.countProperties);
